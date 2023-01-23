ec74ae96b2043da7694c41f6d7f9ba7359e9b149
diasGustaavo <diasgustaavo@icloud.com>
2023-01-23 11:33:46 -0300

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Gustavo Dias on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
